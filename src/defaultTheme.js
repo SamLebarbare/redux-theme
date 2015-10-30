6c087cc9009456ce24061fdeebd22ee33bc413d7
Samuel Loup <loup@epsitec.ch>
2015-10-30 15:24:41 +0100

    Fix default theme

--- src/defaultTheme.js
+++ src/defaultTheme.js
@@ -1,32 +1,50 @@
 import { Colors, ColorManipulator, Transitions } from './utils';
-export default {
-  spacing: {
-    iconSize: 24,
-    desktopKeylineIncrement: 64
-  },
-  typo: {
-    font: 'Roboto, sans-serif',
-    small: '12px',
-    normal: '16px',
-    big: '24px'
-  },
-  shapes: {
-    defaultBorderRadius: '2px'
-  },
-  palette: {
-      primary1Color: Colors.lightBlue500,
-      primary2Color: Colors.lightBlue700,
-      primary3Color: Colors.lightBlue100,
-      accent1Color:  Colors.pinkA200,
-      accent2Color:  Colors.pinkA400,
-      accent3Color:  Colors.pinkA100,
-      textColor:     Colors.darkBlack,
-      subTextColor:  ColorManipulator.fade(Colors.darkBlack, 0.54),
-      canvasColor:   Colors.white,
-      paperColor:    Colors.white,
-      borderColor:   Colors.grey300,
-      disabledColor: ColorManipulator.fade(Colors.darkBlack, 0.3)
-  },
-  colors: Colors,
-  transitions: Transitions
+
+export default class theme {
+  constructor () {
+    this.spacing = {
+      iconSize: 24,
+      desktopKeylineIncrement: 64
+    };
+
+    this.typo = {
+      font: 'Roboto, sans-serif',
+      small: '12px',
+      normal: '16px',
+      big: '24px'
+    };
+
+    this.spacing = {
+      iconSize: 24,
+      desktopKeylineIncrement: 64
+    };
+
+    this.shapes = {
+      defaultBorderRadius: '2px'
+    };
+
+    this.palette = {
+        primary1Color: Colors.lightBlue500,
+        primary2Color: Colors.lightBlue700,
+        primary3Color: Colors.lightBlue100,
+        accent1Color:  Colors.pinkA200,
+        accent2Color:  Colors.pinkA400,
+        accent3Color:  Colors.pinkA100,
+        textColor:     Colors.darkBlack,
+        subTextColor:  ColorManipulator.fade(Colors.darkBlack, 0.54),
+        canvasColor:   Colors.white,
+        paperColor:    Colors.white,
+        borderColor:   Colors.grey300,
+        disabledColor: ColorManipulator.fade(Colors.darkBlack, 0.3)
+    };
+
+    this.shapes = {
+      defaultBorderRadius: '2px'
+    };
+
+    this.colors = Colors;
+
+    this.transitions = Transitions;
+  }
+
 };
--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ export connectTheme  from './themeDecorator';
 export {applyTheme}  from './themeReducer';
 export {configure}   from './themeReducer';
 export { Colors, ColorManipulator, Transitions } from './utils';
-export defaultTheme  from './defaultTheme';
+export theme         from './defaultTheme';
